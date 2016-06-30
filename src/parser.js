37eb7bdb70fea82a05bdcab110034a6f4178430a
Ofek <ofek@outlook.com>
2016-07-01 01:06:53 +0300

    initial pdf musings with pdfmake in mind

--- src/parser.js
+++ src/parser.js
@@ -10,18 +10,19 @@ var parser = {
     } else if (_options === undefined) {
       _options = {};
     }
-    
+
     // Default options
     var options = {
-      tokens: _options['tokens'] || false
+      tokens: _options['tokens'] || false,
+      tokens: _options['pdf'] || false
     };
-      
-    var tokens = tokenizer.tokenize(script), 
+
+    var tokens = tokenizer.tokenize(script),
         token,
-        title_page_html = [], 
+        title_page_html = [],
         script_html = [];
-    
-    var output = { 
+
+    var output = {
       title: '',
       credit: '',
       authors: [],
@@ -31,128 +32,165 @@ var parser = {
       date: '',
       contact: '',
       copyright: '',
-      
+
       scenes: [],
-      
+
       title_page_html: '',
       script_html: ''
     };
+
+    if (options.pdf) {
+      output = {
+        content: [],
+
+        styles: {
+          scene_heading: {
+            fontSize: 14,
+            margin: [0, 0, 0, 4.57]
+          },
+          transition: {
+            margin: [0, 0, 0, 4.57],
+            alignment: 'left'
+          },
+          character: {
+            margin: [0, 0, 60.71, 0]
+          },
+          parenthetical: {
+            margin: [59.94, 0, 53.34, 0]
+          },
+          dialogue: {
+            margin: [39.88, 0, 35.56, 4.57]
+          },
+          action: {
+            margin: [0, 0, 0, 4.57]
+          },
+          centered: {
+            // inherits from 'action'
+            alignment: 'center'
+          }
+        },
+        pageSize: 'A4',
+        pageMargins: [20, 20, 29.97, 12.95],
+        fontSize: 13,
+        alignment: 'right'
+      };
+    }
 
     for (var j in tokens) {
       token = tokens[j];
       token.text = parser.lexer(token.text);
 
       switch (token.type) {
-        case 'title': 
-          title_page_html.push('<h1>' + token.text + '</h1>'); 
-          output.title = token.text.replace('<br />', ' ').replace(/<(?:.|\n)*?>/g, ''); 
+        case 'title':
+          title_page_html.push('<h1>' + token.text + '</h1>');
+          output.title = token.text.replace('<br />', ' ').replace(/<(?:.|\n)*?>/g, '');
           break;
-        case 'credit': 
-          title_page_html.push('<p class="credit">' + token.text + '</p>'); 
+        case 'credit':
+          title_page_html.push('<p class="credit">' + token.text + '</p>');
           output.credit = token.text;
           break;
-        case 'author': 
+        case 'author':
           title_page_html.push('<p class="authors">' + token.text + '</p>');
           output.authors.push(token.text)
           break;
-        case 'authors': 
-          title_page_html.push('<p class="authors">' + token.text + '</p>'); 
+        case 'authors':
+          title_page_html.push('<p class="authors">' + token.text + '</p>');
           output.authors = output.authors.concat(token.text.replace('<br />', "\n").replace(', ', ',').split(/[\n,]/));
           break;
-        case 'source': 
-          title_page_html.push('<p class="source">' + token.text + '</p>'); 
+        case 'source':
+          title_page_html.push('<p class="source">' + token.text + '</p>');
           output.source = (token.text);
           break;
-        case 'notes': 
-          title_page_html.push('<p class="notes">' + token.text + '</p>'); 
+        case 'notes':
+          title_page_html.push('<p class="notes">' + token.text + '</p>');
           output.notes = token.text;
           break;
-        case 'draft_date': 
-          title_page_html.push('<p class="draft-date">' + token.text + '</p>'); 
+        case 'draft_date':
+          title_page_html.push('<p class="draft-date">' + token.text + '</p>');
           output.draft_date = token.text;
           break;
-        case 'date': 
-          title_page_html.push('<p class="date">' + token.text + '</p>'); 
+        case 'date':
+          title_page_html.push('<p class="date">' + token.text + '</p>');
           output.date = token.text;
           break;
-        case 'contact': 
-          title_page_html.push('<p class="contact">' + token.text + '</p>'); 
+        case 'contact':
+          title_page_html.push('<p class="contact">' + token.text + '</p>');
           output.contact = token.text;
           break;
-        case 'copyright': 
-          title_page_html.push('<p class="copyright">' + token.text + '</p>'); 
+        case 'copyright':
+          title_page_html.push('<p class="copyright">' + token.text + '</p>');
           output.copyright = token.text;
           break;
 
-        case 'scene_heading': 
-          script_html.push('<h2' + (token.scene_number ? ' id=\"' + token.scene_number + '\">' : '>') + token.text + '</h2>'); 
+        case 'scene_heading':
+          script_html.push('<h2' + (token.scene_number ? ' id=\"' + token.scene_number + '\">' : '>') + token.text + '</h2>');
           output.scenes.push(token.text);
           break;
-        case 'transition': 
+        case 'transition':
           script_html.push('<p class="transition">' + token.text + '</p>');
           break;
 
-        case 'dual_dialogue_begin': 
-          script_html.push('<div class="dual-dialogue">'); 
+        case 'dual_dialogue_begin':
+          script_html.push('<div class="dual-dialogue">');
           break;
-        case 'dialogue_begin': 
-          script_html.push('<div class="dialogue' + (token.dual ? ' ' + token.dual : '') + '">'); 
+        case 'dialogue_begin':
+          script_html.push('<div class="dialogue' + (token.dual ? ' ' + token.dual : '') + '">');
           break;
-        case 'character': 
-          script_html.push('<h4>' + token.text.replace(/^@/, '') + '</h4>'); 
+        case 'character':
+          script_html.push('<h4>' + token.text.replace(/^@/, '') + '</h4>');
           break;
-        case 'parenthetical': 
-          script_html.push('<p class="parenthetical">' + token.text + '</p>'); 
+        case 'parenthetical':
+          script_html.push('<p class="parenthetical">' + token.text + '</p>');
           break;
-        case 'dialogue': 
-          script_html.push('<p>' + token.text + '</p>'); 
+        case 'dialogue':
+          script_html.push('<p>' + token.text + '</p>');
           break;
-        case 'dialogue_end': 
-          script_html.push('</div>'); 
+        case 'dialogue_end':
+          script_html.push('</div>');
           break;
-        case 'dual_dialogue_end': 
-          script_html.push('</div>'); 
-          break;
-
-        case 'section': 
-          script_html.push('<p class="section" data-depth="' + token.depth + '">' + token.text + '</p>'); 
-          break;
-        case 'synopsis': 
-          script_html.push('<p class="synopsis">' + token.text + '</p>'); 
+        case 'dual_dialogue_end':
+          script_html.push('</div>');
           break;
 
-        case 'note': 
-          script_html.push('<!-- ' + token.text + ' -->'); 
+        case 'section':
+          script_html.push('<p class="section" data-depth="' + token.depth + '">' + token.text + '</p>');
+          break;
+        case 'synopsis':
+          script_html.push('<p class="synopsis">' + token.text + '</p>');
+          break;
+
+        case 'note':
+          script_html.push('<!-- ' + token.text + ' -->');
           break;
         case 'boneyard_begin':
           script_html.push('<!-- ');
           break;
-        case 'boneyard_end': 
-          script_html.push(' -->'); 
+        case 'boneyard_end':
+          script_html.push(' -->');
           break;
-        
+
         case 'lyrics':
           script_html.push('<p class="lyrics">' + token.text + '</p>');
           break;
-        case 'action': 
-          script_html.push('<p>' + token.text + '</p>'); 
+        case 'action':
+          script_html.push('<p>' + token.text + '</p>');
           break;
-        case 'centered': 
-          script_html.push('<p class="centered">' + token.text + '</p>'); 
+        case 'centered':
+          script_html.push('<p class="centered">' + token.text + '</p>');
           break;
-        
-        case 'page_break': 
-          script_html.push('<hr />'); 
+
+        case 'page_break':
+          script_html.push('<hr />');
           break;
-        case 'line_break': 
-          script_html.push('<br />'); 
+        case 'line_break':
+          script_html.push('<br />');
           break;
       }
     }
 
     output.title_page_html = title_page_html.join('');
     output.script_html = script_html.join('');
-    
+
     if (options.tokens) {
       output.tokens = tokens;
     };
@@ -163,16 +201,16 @@ var parser = {
 
     return output;
   },
-  
-  
-  
+
+
+
 
 
   lexer: function (s) {
     if (!s) {
       return;
-    }  
-    
+    }
+
     var inline = {
       note: '<!-- $1 -->',
       line_break: '<br />',
@@ -186,7 +224,7 @@ var parser = {
     };
 
     var styles = ['bold_italic_underline', 'bold_underline', 'italic_underline', 'bold_italic', 'bold', 'italic', 'underline'],
-        style, 
+        style,
         match;
 
     s = s.replace(sections.note_inline, inline.note).replace(/\\\*/g, '[star]').replace(/\\_/g, '[underline]').replace(/\n/g, inline.line_break);
@@ -194,7 +232,7 @@ var parser = {
     for (var i in styles) {
       style = styles[i];
       match = sections[style];
- 
+
       if (match.test(s)) {
         s = s.replace(match, inline[style]);
       }
@@ -202,7 +240,7 @@ var parser = {
 
     return s.replace(/\[star\]/g, '*').replace(/\[underline\]/g, '_').trim();
   }
-  
+
 };
 
 
